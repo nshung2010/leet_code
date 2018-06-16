3967e7801a7db823a2d0d8417c23efca17b98865
nshung2010 <hungns2010@gmail.com>
2018-06-16 14:30:22 -0400

    update 44 wildcard_match with Finite State Machine algorithm

--- 44_wildcard_matching.py
+++ 44_wildcard_matching.py
@@ -1,10 +1,11 @@
+"""
 class Solution(object):
     def isMatch(self, s, p):
-        """
+        
         :type s: str
         :type p: str
         :rtype: bool
-        """
+        
         m, n = len(s), len(p)
         dp=[[False]*(n+1) for _ in range(m+1)]
         dp[0][0] = True
@@ -19,6 +20,38 @@ class Solution(object):
                     last_match = p[j] in {s[i], "?"}
                     dp[i+1][j+1]= last_match and dp[i][j]
         return dp[m][n]
+
+A = Solution()
+c = A.isMatch("adceb", "*a*b")
+print(c)
+"""
+
+class Solution(object):
+    def isMatch(self, s, p):
+        """
+        :type s: str
+        :type p: str
+        :rtype: bool
+        """
+        transfer = {}
+        state = 0
+        
+        for char in p:
+            if char == '*':
+                transfer[state, char] = state
+            else:
+                transfer[state, char] = state + 1
+                state += 1
+        
+        accept = state
+        print(accept)
+        print(transfer)
+        state = set([0])
+        print(state)
+        for char in s:
+            state = set([transfer.get((at, token)) for at in state for token in [char, '*', '?']])
+        print(state)
+        return accept in state
 
 A = Solution()
 c = A.isMatch("adceb", "*a*b")
