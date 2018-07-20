54da0a5693b483c422a00ca7d94ad12d5ad36840
nshung2010 <hungns2010@gmail.com>
2018-07-19 23:21:07 -0400

    add Q 88

--- leetcodepractice.py
+++ leetcodepractice.py
@@ -3241,7 +3241,7 @@ class Solution(object):
         
         return head_less.next
 
-#86 Scramble string
+#87 Scramble string
 class Solution(object):
     def isScramble(self, s1, s2):
         """
@@ -3262,6 +3262,36 @@ class Solution(object):
         return False
 
 
-
+#88 Merge sorted array
    	
-    
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: void Do not return anything, modify nums1 in-place instead.
+        """
+        if m==0:
+            nums1[0:n] = nums2
+            return
+        if n==0:
+            return
+        if nums2[n-1] < nums1[0]:
+            nums1[0:m+n] = nums2 + nums1[0:m]
+            return
+        if nums1[m-1] < nums2[0]:
+            nums1[0:m+n] = nums1[0:m] + nums2
+        i = m-1
+        j = n-1
+        
+        while i>=0 and j>=0:
+            if nums1[i] < nums2[j]:
+                nums1[i+j+1] = nums2[j]
+                j -= 1
+            else:
+                nums1[i+j+1] = nums1[i]
+                i -= 1
+        while j>= 0:
+            nums1[j] = nums2[j]   
