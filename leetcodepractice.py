38e44711d23c5bb0eb22dc556b6b9d32b6d5f3cb
nshung2010 <hungns2010@gmail.com>
2018-07-21 00:23:32 -0400

    fisnish Q88

--- leetcodepractice.py
+++ leetcodepractice.py
@@ -3283,15 +3283,11 @@ class Solution(object):
             return
         if nums1[m-1] < nums2[0]:
             nums1[0:m+n] = nums1[0:m] + nums2
-        i = m-1
-        j = n-1
+            return
+        if nums1[m-1]<nums2[n-1]:
+            nums1[m+n-1] = nums2[n-1]
+            self.merge(nums1, m, nums2[:n-1], n-1)
+        else:
+            nums1[m+n-1] = nums1[m-1]
+            self.merge(nums1, m-1, nums2, n)
         
-        while i>=0 and j>=0:
-            if nums1[i] < nums2[j]:
-                nums1[i+j+1] = nums2[j]
-                j -= 1
-            else:
-                nums1[i+j+1] = nums1[i]
-                i -= 1
-        while j>= 0:
-            nums1[j] = nums2[j]   
