62fe4a6a2c4af355b26808322df11af5db39eb7c
nshung2010 <hungns2010@gmail.com>
2019-06-07 22:23:19 -0400

    practice

--- test_1.py
+++ test_1.py
@@ -1,3 +1,4 @@
+import numpy as np
 a = [7, 3, 2, 4, 6]
 b = [4, 2, 5, 6]
 for _ in a+b:
@@ -5,3 +6,8 @@ for _ in a+b:
     print(temp)
 #s = [max(a, b).pop(0) for _ in a+b]
 # print(s)
+a = np.array([[1, 2, 3], [2, 3, 4]])
+print(a)
+cal = np.sum(a, axis=0)
+print(cal)
+print(100*a/cal)
